e818f5719cb7794510384f74b3e8f6e7cc636b83
Shreena1996 <51013605+Shreena1996@users.noreply.github.com>
2019-06-15 15:26:18 -0500

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,91 @@
+<div class="sidebar app-aside" id="sidebar">
+				<div class="sidebar-container perfect-scrollbar">
+
+<nav>
+						
+						<!-- start: MAIN NAVIGATION MENU -->
+						<div class="navbar-title">
+							<span>Main Navigation</span>
+						</div>
+						<ul class="main-navigation-menu">
+							<li>
+								<a href="dashboard.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-home"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Dashboard </span>
+										</div>
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="book-appointment.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-pencil-alt"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Book Appointment </span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+							<li>
+								<a href="appointment-history.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-list"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Appointment History </span>
+										</div>
+									</div>
+								</a>
+							</li>
+							
+								<li>
+								<a href="https://askthedentist.com/">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-medall"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Ask a Dentist </span>
+										</div>
+									</div>
+								</a>
+							</li>
+								<li>
+								<a href="https://islingtondentalclinic.com/blog-page-1">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-desktop"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> View Blog </span>
+										</div>
+									</div>
+								</a>
+							</li>
+								<li>
+								<a href="delivery.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-shopping-cart"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Order Medicine Now </span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+						</ul>
+						<!-- end: CORE FEATURES -->
+						
+					</nav>
+					</div>
+			</div>
